221669815ee5faab4e1869a7caca737a44643edf
adam-bates <adam@adambates.ca>
2022-08-28 09:57:59 -0400

    Initial commit.

---
+++ examples/shared_state.rs
@@ -0,0 +1,22 @@
+use ferrum_runtime::prelude::*;
+
+fn main() {
+    let name: FeStr = FeStr::from("Adam");
+    print(&name);
+
+    let name1: FeBox<FeStr> = FeBox::new(name);
+    print(&name1);
+
+    let mut name2: FeBox<FeStr> = FeBox::share(&name1);
+
+    let mut name3: FeBox<FeStr> = FeBox::share(&name1);
+
+    print(&name1);
+
+    *name2 = FeStr::from("Foo");
+    print(&name1);
+
+    *name3 = FeStr::from("Bar");
+    print(name1);
+}
+

e81ef533f4624301323f77a731a17d87faf9f8b6
adam-bates <adam@adambates.ca>
2022-08-29 00:28:02 -0400

    trying to fix syn parsing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-pub mod fe_std;
-pub mod prelude;
+mod fe_prelude;
+mod fe_std;
 mod utils;
+
+pub mod prelude {
+    pub use super::fe_prelude::*;
+}
+
+pub mod std {
+    pub use super::fe_std::*;
+}
 
